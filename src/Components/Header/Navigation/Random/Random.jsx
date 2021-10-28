f1f1c00229c359b76798f687ecb21805465bca56
Caroline COPY-DENHEZ <caroline.copy19@gmail.com>
2021-10-28 12:19:34 +0200

    All the fonts were also moved to the public folder as they were part of the assets folder

--- src/Components/Header/Navigation/Random/Random.jsx
+++ src/Components/Header/Navigation/Random/Random.jsx
@@ -2,6 +2,7 @@ const Random = () => {
   return (
     <li>
       <img
+        className="random"
         src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-random-interface-kiranshastry-solid-kiranshastry.png"
         alt="random"
       />
