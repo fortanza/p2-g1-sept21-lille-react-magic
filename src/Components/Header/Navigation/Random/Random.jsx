bb81a1bd39aef074eec306e1b141635580893b71
Caroline COPY-DENHEZ <caroline.copy19@gmail.com>
2021-10-28 13:20:22 +0200

    Code improvements

--- src/Components/Header/Navigation/Random/Random.jsx
+++ src/Components/Header/Navigation/Random/Random.jsx
@@ -1,11 +1,7 @@
 const Random = () => {
   return (
     <li>
-      <img
-        className="random"
-        src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-random-interface-kiranshastry-solid-kiranshastry.png"
-        alt="random"
-      />
+      <img className="random" src="/assets/img/random.png" alt="random" />
     </li>
   );
 };
