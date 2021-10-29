f8d2ac13621437232c62b438741e56135d21d7b2
NacymeG <nacyme.gallel@gmail.com>
2021-10-29 21:52:48 +0200

    Tweaked SectionSearch

--- src/Components/SectionSearch/SectionSearch.jsx
+++ src/Components/SectionSearch/SectionSearch.jsx
@@ -1,9 +1,11 @@
+import RotateLogo from './RotateLogo/RotateLogo';
 import SearchBar from './SearchBar/SearchBar';
 import './SectionSearch.css';
 
 export default function SectionSearch() {
   return (
     <section className="Section_searchbar">
+      <RotateLogo />
       <SearchBar />
     </section>
   );
