0e4cace0eff0c5912694aa54f4a0562684919804
NacymeG <nacyme.gallel@gmail.com>
2021-11-04 16:35:59 +0100

    Added a path to the card description

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from 'Components/Header/Header';
 import Footer from 'Components/Footer/Footer';
 import { Route, Switch } from 'react-router-dom';
 import Home from 'Containers/Home';
+import CardDescription from 'Containers/CardDescription';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/Cards/" component={CardDescription} />
       </Switch>
       <Footer />
     </div>
