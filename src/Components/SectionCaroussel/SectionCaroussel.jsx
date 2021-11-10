d818f59bebdc64eeb4600e53906670b90efa528f
NacymeG <nacyme.gallel@gmail.com>
2021-11-10 16:33:26 +0100

    Carousel is now dynamic

--- src/Components/SectionCaroussel/SectionCaroussel.jsx
+++ src/Components/SectionCaroussel/SectionCaroussel.jsx
@@ -1,20 +1,38 @@
 import { Carousel } from '3d-react-carousal';
 import './SectionCaroussel.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const cards = [409741, 409742, 409743, 409744, 409745];
 const SlidesCaroussel = () => {
+  const [cardLists, setCardLists] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.magicthegathering.io/v1/cards?random=true&set=MID&page=1&pageSize=100`
+      )
+      .then(({ data }) => {
+        setCardLists(data.cards);
+      });
+  }, []);
+
+  // const result = cardLists.filter((card) => card.imageUrl);
+
   return (
     <section className="sectionCaroussel">
       <div>
         <Carousel
-          slides={cards.map((card) => {
-            return (
-              <img
-                src={`http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${card}&type=card`}
-                alt="a card slide"
-              />
-            );
-          })}
+          slides={cardLists
+            .filter((e) => {
+              return e.imageUrl != null;
+            })
+            .map((card) => {
+              return (
+                <>
+                  <img src={card.imageUrl} alt="a card slide" />
+                </>
+              );
+            })}
         />
       </div>
     </section>
