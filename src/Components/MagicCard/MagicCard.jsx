b22ef6653224437246a0d04710beb8f52eea15a0
NacymeG <nacyme.gallel@gmail.com>
2021-11-10 16:31:22 +0100

    Tweaked card border and removed unused button

--- src/Components/MagicCard/MagicCard.jsx
+++ src/Components/MagicCard/MagicCard.jsx
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 
 const MagicCard = ({ name, imageUrl }) => {
   return (
-    <figure className="card">
-      <h3>{name}</h3>
-      <img src={imageUrl} alt={name} />
-      <button className="myButton" type="button">
-        Description
-      </button>
-      <button className="myButton" type="button">
-        Add to Deck
-      </button>
-    </figure>
+    <>
+      {imageUrl ? (
+        <figure className="card">
+          <h3>{name}</h3>
+          <img src={imageUrl} alt={name} />
+        </figure>
+      ) : null}
+    </>
   );
 };
 
@@ -22,8 +20,7 @@ MagicCard.propTypes = {
 };
 
 MagicCard.defaultProps = {
-  name: 'Archangel Avacyn',
-  imageUrl:
-    'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=409741&type=card',
+  name: '',
+  imageUrl: '',
 };
 export default MagicCard;
