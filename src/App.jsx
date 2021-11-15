3c249dad969bdbf2cb9d8f8b8d0c8ce184ba716b
NacymeG <nacyme.gallel@gmail.com>
2021-11-15 20:00:03 +0100

    Add route for the filter and cardlist to carddesc

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import Home from 'Containers/Home';
 import SearchCards from 'Containers/SearchCards/SearchCards';
 import CardDescription from 'Containers/CardDescription';
+import FilterOption from 'Containers/FilterOption';
+import SearchCardsAdvanced from 'Containers/SearchCards/SearchCardsAdvanced';
 import './App.css';
 
 function App() {
@@ -17,6 +19,15 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/searchCards/:needle" component={SearchCards} />
         <Route path="/card" component={CardDescription} />
+        <Route
+          path="/CardDescription/:multiverseid"
+          component={CardDescription}
+        />
+        <Route path="/filter" component={FilterOption} />
+        <Route
+          path="/SearchCardsAdvanced/:needleName/:needlePower/:needleColor/:needleRarity/:needleSet"
+          component={SearchCardsAdvanced}
+        />
       </Switch>
       <Footer />
     </div>
