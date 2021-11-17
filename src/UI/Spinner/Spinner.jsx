7f86e1abb1318910e43115084689223fd58f6536
NacymeG <nacyme.gallel@gmail.com>
2021-11-17 16:57:58 +0100

    Spinner fixed

--- src/UI/Spinner/Spinner.jsx
+++ src/UI/Spinner/Spinner.jsx
@@ -2,12 +2,14 @@ import './Spinner.css';
 
 const Spinner = () => {
   return (
-    <div className="loader loader--fan-out">
-      <div className="item item-1" />
-      <div className="item item-2" />
-      <div className="item item-3" />
-      <div className="item item-4" />
-      <div className="item item-5" />
+    <div className="loaderWrapper">
+      <div className="loader loader--fan-out">
+        <div className="item item-1" />
+        <div className="item item-2" />
+        <div className="item item-3" />
+        <div className="item item-4" />
+        <div className="item item-5" />
+      </div>
     </div>
   );
 };
