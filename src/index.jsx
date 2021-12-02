2783e3b1db93d53a19f16b3fd12718325be25440
Cédric Lassalle <67291627+fortanza@users.noreply.github.com>
2021-12-02 08:05:08 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/magiccards/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
